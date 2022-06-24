fcb08a2995820a7746dacfa9f6dbceee802d012e
Efra-Ls <104372088+Efra-Ls@users.noreply.github.com>
2022-06-24 18:32:34 -0500

    robot

--- proyecto/html/blog/Footer.php
+++ proyecto/html/blog/Footer.php
@@ -1,6 +1,7 @@
 <!--Este documento contiene el footer del sitio web-->
 <!DOCTYPE html>
-<html lang="en"> 
+<html lang="en">
+<script type='text/javascript' data-cfasync='false'>window.purechatApi = { l: [], t: [], on: function () { this.l.push(arguments); } }; (function () { var done = false; var script = document.createElement('script'); script.async = true; script.type = 'text/javascript'; script.src = 'https://app.purechat.com/VisitorWidget/WidgetScript'; document.getElementsByTagName('HEAD').item(0).appendChild(script); script.onreadystatechange = script.onload = function (e) { if (!done && (!this.readyState || this.readyState == 'loaded' || this.readyState == 'complete')) { var w = new PCWidget({c: '52e1b770-1df4-40b5-82f7-8d71e4147973', f: true }); done = true; } }; })();</script> 
     <footer> 
         <!-- Copyright Start -->
         <div class="container-fluid copyright text-light py-4 wow fadeIn" data-wow-delay="0.1s">
